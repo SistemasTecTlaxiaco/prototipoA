f09622d29df72f16edcc0f8a8adefb900f874628
Arnold-Js <79930202+Arnold-Js@users.noreply.github.com>
2021-03-03 12:32:40 -0600

    Initialize Desarrollo

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('URL', 'https://oaxaqueando1.herokuapp.com/');
+    define('HOST', 'bdyzcfehrcomfhksbhmx-mysql.services.clever-cloud.com');
+    define('DB', 'bdyzcfehrcomfhksbhmx');
+    define('USER', 'up1bqfux0eibfreq');
+    define('PASSWORD', 'Oe4HhihpfWtMz8oLw7DZ');
+    define('CHARSET', 'utf8mb4');
+?>
